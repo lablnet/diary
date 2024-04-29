d45a48da6b5d3f320a7d786b9c5c03dddd9ed498
Muhammad Umer Farooq <umer@lablnet.com>
2024-04-29 17:23:25 +0500

    Integrate nprogress
    Signed-off-by: Muhammad Umer Farooq <umer@lablnet.com>

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,9 +1,37 @@
-import { createRouter, createWebHashHistory, Router } from 'vue-router'
+import { createRouter, createWebHashHistory, Router, RouteLocationNormalized, NavigationGuardNext } from 'vue-router'
 import routes from './routes'
+
+// Nprogress
+import Nprogress from 'nprogress'
+// Nprogress CSS
+import 'nprogress/nprogress.css'
 
 const router : Router = createRouter({
   history: createWebHashHistory(),
   routes
 })
+
+router.beforeEach(
+  (
+    to: RouteLocationNormalized,
+    from: RouteLocationNormalized,
+    next: NavigationGuardNext
+  ) => {
+    // Start the animation of the route progress bar.
+    Nprogress.start()
+
+    next()
+  }
+)
+
+router.afterEach(
+  (
+    to: RouteLocationNormalized,
+    from: RouteLocationNormalized
+  ) => {
+    // Complete the animation of the route progress bar.
+    Nprogress.done()
+  }
+)
 
 export default router
