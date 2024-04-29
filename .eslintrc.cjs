59476fc9a0d1b1be4798f8d87f0d3e9b3082cd81
Muhammad Umer Farooq <umer@lablnet.com>
2024-04-29 20:59:35 +0500

    Update ESLint configuration and dependencies

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,22 +1,41 @@
 module.exports = {
     root: true,
     env: {
-      node: true
+        node: true
     },
-    'extends': [
-      'plugin:vue/essential',
-      'eslint:recommended'
+    extends: [
+        'plugin:vue/essential',
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended'
     ],
     parserOptions: {
-      parser: 'babel-eslint',
-      requireConfigFile: false,
-      ecmaVersion: 8,  
+        parser: '@typescript-eslint/parser',
+        requireConfigFile: false,
+        ecmaVersion: 8
     },
     rules: {
-      'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-      'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-      'no-console': process.env.NODE_ENV === 'production' ? 2 : 1,
-      'no-unused-vars': 'off',
-      'no-case-declarations': 'off',
-    }
-}
+        'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+        'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+        'no-console': process.env.NODE_ENV === 'production' ? 2 : 1,
+        'no-unused-vars': 'off',
+        'no-case-declarations': 'off',
+        '@typescript-eslint/no-inferrable-types': 'off',
+        '@typescript-eslint/ban-ts-comment': 'off'
+    },
+    plugins: ['@typescript-eslint'],
+    overrides: [
+        {
+            files: ['*.vue'],
+            rules: {
+                '@typescript-eslint/no-inferrable-types': 'off',
+                '@typescript-eslint/ban-ts-comment': 'off'
+            },
+            parser: 'vue-eslint-parser',
+            parserOptions: {
+                parser: '@typescript-eslint/parser',
+                ecmaVersion: 2020,
+                sourceType: 'module'
+            }
+        }
+    ]
+};
