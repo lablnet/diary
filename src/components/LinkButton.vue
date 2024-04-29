e30afd3df3f7126d587853fdc217e041c7271479
Muhammad Umer Farooq <umer@lablnet.com>
2024-04-29 17:53:42 +0500

    Added LinkButton Component
    Signed-off-by: Muhammad Umer Farooq <umer@lablnet.com>

---
+++ src/components/LinkButton.vue
@@ -0,0 +1,43 @@
+<template>
+    <div :class="divClass">
+        <router-link
+        :to="link"
+        :class="
+            disable
+            ? `${_class} opacity-25 cursor-not-allowed`
+            : `${_class} cursor-pointer`
+        "
+        >
+        {{ text }}
+        </router-link>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+export default defineComponent({
+    name: "LinkButton",
+    props: {
+        divClass: {
+            type: String,
+            default: ""
+        },
+        _class: {
+            type: String,
+            default: "text-sm font-medium text-primary hover:underline dark:text-primary"
+        },
+        text: {
+            type: String,
+            default: "Button"
+        },
+        link: {
+            type: String,
+            default: null
+        },
+        disable: {
+            type: Boolean,
+            default: false
+        }
+    },
+});
+</script>
