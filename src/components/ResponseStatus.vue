560ae8df55bc3bdd9dd83eef06c0f4f4534edbea
Muhammad Umer Farooq <umer@lablnet.com>
2024-04-29 17:55:35 +0500

    Added ResponseStatus Component
    Signed-off-by: Muhammad Umer Farooq <umer@lablnet.com>

---
+++ src/components/ResponseStatus.vue
@@ -0,0 +1,39 @@
+<template>
+  <div
+    v-if="response"
+    class="mt-2 mb-2 text-sm font-medium p-4 w-full"
+    :class="alertClass"
+  >
+    {{ response }}
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, computed, watch } from "vue";
+
+export default defineComponent({
+  name: "ResponseStatus",
+  props: {
+    error: { type: String, default: null },
+    success: { type: String, default: null },
+  },
+
+  setup(props) {
+    const response = computed(() => {
+      return props.success || props.error || null;
+    });
+
+    const alertClass = computed(() => {
+      if (props.success) {
+        return "bg-green-600 text-white";
+      }
+      if (props.error) {
+        return "bg-red-600 text-white";
+      }
+      return null;
+    });
+
+    return { response, alertClass };
+  },
+});
+</script>
