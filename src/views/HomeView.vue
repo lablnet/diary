3bad1e9ff714b6d30e642ff5c0fea9e919f0835b
Muhammad Umer Farooq <umer@lablnet.com>
2024-04-29 17:00:40 +0500

    Integrate TailwindCSS
    Signed-off-by: Muhammad Umer Farooq <umer@lablnet.com>

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,10 @@
 <template>
   <div class="home">
+    <p class="bg-red-500">
+      For a guide and recipes on how to configure / customize this project,<br>
+      check out the
+      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
+    </p>
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
   </div>
