8981b2137b86d4788e5e74be96ba74e2cde6924b
Muhammad Umer Farooq <umer@lablnet.com>
2024-04-29 17:55:12 +0500

    Added PrimaryButton Component
    Signed-off-by: Muhammad Umer Farooq <umer@lablnet.com>

---
+++ src/components/PrimaryButton.vue
@@ -0,0 +1,55 @@
+<template>
+  <div :class="divClass">
+    <button
+      @click="handleClick"
+      :disabled="disable"
+      :class="[
+        disable
+          ? `${_class} opacity-25 cursor-not-allowed`
+          : `${_class} cursor-pointer`,
+      ]"
+    >
+      {{ text }}
+    </button>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "PrimaryButton",
+  props: {
+    divClass: {
+      type: String,
+      default: "",
+    },
+    _class: {
+      type: String,
+      default:
+        "w-full text-white bg-primary focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center",
+    },
+    text: {
+      type: String,
+      default: "Button",
+    },
+    link: {
+      type: String,
+      default: null,
+    },
+    disable: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  setup(props, { emit }) {
+    const handleClick = () => {
+      emit("handleOnClick");
+    };
+
+    return {
+      handleClick,
+    };
+  },
+});
+</script>
