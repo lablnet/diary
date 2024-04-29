022915d6420130e041f2eb75309b977bb5e54ab0
Muhammad Umer Farooq <umer@lablnet.com>
2024-04-29 17:05:55 +0500

    Added utils function to handle theme
    Signed-off-by: Muhammad Umer Farooq <umer@lablnet.com>

---
+++ src/utils/theme.ts
@@ -0,0 +1,24 @@
+/**
+ * Function to set the theme.
+ * @since v1.0.0
+ * @returns null
+ */
+export const themeSetup = () => {
+    const appTheme = localStorage.getItem('theme') || 'light';
+
+      if (
+        appTheme === 'dark' &&
+        // @ts-ignore: Object is possibly 'null'.
+        document.querySelector('body').classList.contains('app-theme')
+      ) {
+        // @ts-ignore: Object is possibly 'null'.
+        document.querySelector('body').classList.remove('bg-secondary-light');
+        // @ts-ignore: Object is possibly 'null'.
+        document.querySelector('body').classList.add('bg-primary-dark');
+      } else {
+        // @ts-ignore: Object is possibly 'null'.
+        document.querySelector('body').classList.remove('bg-primary-dark');
+        // @ts-ignore: Object is possibly 'null'.
+        document.querySelector('body').classList.add('bg-secondary-light');
+      }
+}
