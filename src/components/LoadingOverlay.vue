66a16a2a0549cb2653aa928838daf85f0e92f138
Muhammad Umer Farooq <umer@lablnet.com>
2024-04-29 17:54:36 +0500

    Added LoadingOverlay Component
    Signed-off-by: Muhammad Umer Farooq <umer@lablnet.com>

---
+++ src/components/LoadingOverlay.vue
@@ -0,0 +1,21 @@
+<template>
+  <div
+    id="loader"
+    class="fixed inset-0 min-h-screen flex items-center justify-center bg-white bg-opacity-95 z-10"
+    v-if="loading"
+  >
+    <i class="fa fa-spinner fa-spin text-blue-500"> </i>
+    Loading do not close the browser
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "LoadingOverlay",
+  props: {
+    loading: Boolean,
+  },
+});
+</script>
