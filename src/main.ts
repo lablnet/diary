2909f1ab03013f31b458085ae0cbb0146fb7e671
Muhammad Umer Farooq <umer@lablnet.com>
2024-04-29 17:58:01 +0500

    Install pinia
    Signed-off-by: Muhammad Umer Farooq <umer@lablnet.com>

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { createApp, App as VueApp } from "vue";
 import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
+import { createPinia } from "pinia";
 import { auth } from '@/services/firebase'
 
 import '@/assets/styles/index.css'
@@ -10,7 +11,7 @@ let app: VueApp | null = null;
 
 auth.onAuthStateChanged((user) => {
     if (!app) {
-        app = createApp(App);
-        app.use(router).mount('#app')
+        app = createApp(App)
+        app.use(createPinia()).use(router).mount('#app')
     }
 });
