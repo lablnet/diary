f1cb560af4ed44ad6b242f33d7ab9a890111927a
Muhammad Umer Farooq <umer@lablnet.com>
2024-04-29 17:28:10 +0500

    Refactor
    Signed-off-by: Muhammad Umer Farooq <umer@lablnet.com>

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,18 +6,5 @@
       <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
     </p>
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
   </div>
 </template>
-
-<script lang="ts">
-import { Options, Vue } from 'vue-class-component'
-import HelloWorld from '@/components/HelloWorld.vue' // @ is an alias to /src
-
-@Options({
-  components: {
-    HelloWorld
-  }
-})
-export default class HomeView extends Vue {}
-</script>
