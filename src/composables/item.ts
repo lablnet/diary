93735054c312a7dcc7459fcc2160d4fe20ef7aea
Muhammad Umer Farooq <umer@lablnet.com>
2024-04-30 10:30:45 +0500

    Added composeable item
    Signed-off-by: Muhammad Umer Farooq <umer@lablnet.com>

---
+++ src/composables/item.ts
@@ -0,0 +1,27 @@
+import { ref } from 'vue'
+import { shouldNotEmpty, handleError } from '@/utils'
+import { ItemData } from '@/types/item'
+import { ItemService } from '@/services/item_service'
+
+export function useItem () {
+  const item = ref<ItemData | null>(null)
+  const loading = ref<boolean>(false)
+
+  async function getItem (id: string) {
+    try {
+      loading.value = true
+      const itemData = await (new ItemService()).getItem(id)
+      item.value = itemData
+    } catch (error) {
+      handleError(error)
+    } finally {
+      loading.value = false
+    }
+  }
+
+  return {
+    item,
+    loading,
+    getItem
+  }
+}
